50831fa966520b2195335150902e93c85bde428e
akif <akif.dere@rlcteknoloji.com>
2021-01-19 21:17:50 +0300

    Initial Commit

---
+++ testApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testApp
+//
+//  Created by Mehmet Akif DERE on 19.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
